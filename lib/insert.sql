3edc60ca556926d9dd21e8684514cbb345086ec4
Chris Lozzi <clozzi12@gmail.com>
2023-12-12 12:40:17 -0500

    all tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'F', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, 'M', 'Brown', 'Sweet', 1);
